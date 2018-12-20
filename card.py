19ac5d80ce65bd5e314c77d79cbac35891426e0a
Jhonatan Lopes <jhonatan.jal@gmail.com>
2018-12-20 17:23:21 -0300

    Card to anki txt format

--- card.py
+++ card.py
@@ -1,8 +1,25 @@
+import re
+
+
 class Card:
     """Represents the card model"""
 
-    def __init__(self, word, pronunciation, definition, examples):
+    def __init__(self, word, pronunciation, definition, example):
         self.word = word
         self.pronunciation = pronunciation
         self.definition = definition
-        self.examples = examples
+        self.example = example
+
+    #TODO to think of a better name for the method. __word_in_example_to_bold(self)?
+    def __wrap_in_bold_tag(self): 
+        """
+        Finds the word of the card in the example and wrap it in a bold's tag
+        """
+        word = re.search(f'{self.word}.*?\w*', self.example).group()
+        return self.example.replace(word, f'<b>{word}</b>')
+
+    def to_anki_txt_format(self):
+        """Return the card in a string format that the Anki can imports"""
+        return (f'{self.__wrap_in_bold_tag()}; "<b>{self.word}'
+                f' /{self.pronunciation}/</b><br> {self.definition}"')
+
