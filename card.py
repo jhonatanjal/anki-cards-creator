5d9e6105a29703f383678dc50f526cfe73951bea
Jhonatan Lopes <jhonatan.jal@gmail.com>
2018-12-20 13:33:25 -0300

    initial commit

---
+++ card.py
@@ -0,0 +1,8 @@
+class Card:
+    """Represents the card model"""
+
+    def __init__(self, word, pronunciation, definition, examples):
+        self.word = word
+        self.pronunciation = pronunciation
+        self.definition = definition
+        self.examples = examples
